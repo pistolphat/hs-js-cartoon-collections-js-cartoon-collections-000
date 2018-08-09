bd7f410db784a9ed6286c70686236c471505d98c
Phat Doan <phatdoan@yahoo.com>
2018-08-09 18:18:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Function 1 - Dwarf Roll Call
 function dwarfRollCall(dwarves) {
   for(var i =0; i < dwarves.length; i++) {
@@ -38,5 +37,3 @@ function findTheCheese (foods) {
     return 'no cheese!';
   }
 }
-=======
->>>>>>> 056b58fa9b90834612d29f89d830d697a86f5cdc
